03f24a581013cd9185e6e8fb7f5e0ca0022afa87
mm7490 <muellnermarco@gmail.com>
2018-08-15 16:54:26 +0200

    Restructured cdo file --> moved meta stuff into its own namespace

--- discord_handler/cdos.py
+++ discord_handler/cdos.py
@@ -1,14 +1,13 @@
 import re
 from typing import Dict, Union
 import logging
-from discord import Message, Channel, Embed
 from collections import OrderedDict
 from django.core.exceptions import ObjectDoesNotExist
-import asyncio
 
-from database.models import CompetitionWatcher, Competition, DiscordUsers, MatchEvents, MatchEventIcon
+from database.models import CompetitionWatcher, Competition, MatchEvents, MatchEventIcon
 from discord_handler.handler import client, watchCompetition
-from inspect import getmembers, isroutine
+from discord_handler.cdo_meta import markCommando, CDOInteralResponseData, cmdHandler, emojiList, DiscordCommando
+
 from support.helper import Task
 
 logger = logging.getLogger(__name__)
@@ -23,176 +22,6 @@ All commandos belong to a certain group and has a certain userlevel associated t
 associated, it will use the GrpGeneral object as its group. Also, if no userlevel is associated to a commando,
 the userlevel of the group is used.
 """
-
-discordCommandos = []
-commandoGroups = []
-emojiList = ["0⃣", "1⃣", "2⃣", "3⃣"]
-
-
-class CommandoGroup:
-    def __init__(self, group, fun: callable, docstring: str, userlevel: int = 0):
-        self.group = group
-        self.fun = fun
-        self.docstring = docstring
-        self.userLevel = userlevel
-        self.associatedCommandos = []
-
-    @staticmethod
-    def allGroups():
-        return commandoGroups
-
-    @staticmethod
-    def addGroup(group):
-        logger.info(f"Adding group {group}")
-        commandoGroups.append(group)
-
-    @staticmethod
-    def associateCommando(commando, group):
-        for i in commandoGroups:
-            if i.fun == group:
-                i.associatedCommandos.append(commando)
-                return i
-
-    def __str__(self):
-        return f"Group {self.group}, userLevel {self.userLevel}"
-
-
-class DiscordCommando:
-    def __init__(self, commando: str, fun: callable, docstring: str, group, userLevel: int):
-        self.commando = commando
-        self.cmd_group = CommandoGroup.associateCommando(self, group)
-        self.userLevel = userLevel if userLevel is not None else self.cmd_group.userLevel
-        self.fun = fun
-        self.docstring = docstring
-
-    @staticmethod
-    def allCommandos():
-        return discordCommandos
-
-    @staticmethod
-    def addCommando(commando):
-        logger.info(f"Add commando {commando}")
-        discordCommandos.append(commando)
-
-    def __str__(self):
-        return f"Cmd {self.cmd_group}:{self.commando}, userLevel {self.userLevel}"
-
-
-################################### Group Helpers #####################################
-
-neededParameters = {'name': str,
-                    'userLevel': int,
-                    }
-
-
-def markGroup(group: str):
-    def internal_func_wrapper(func: callable):
-        attributes = getmembers(func, lambda a: not (isroutine(a)))
-        memberDescriptors = dict([a for a in attributes if not (a[0].startswith('__') and a[0].endswith('__'))])
-        for name, valueType in neededParameters.items():
-            if name not in memberDescriptors.keys():
-                raise AttributeError(f"A group for a command has to implement {name}")
-
-            try:
-                valueType(memberDescriptors[name])
-            except ValueError:
-                raise AttributeError(f"The parameter {name} has to be of type {valueType}")
-        CommandoGroup.addGroup(CommandoGroup(group, func, func.__doc__))
-        return func
-
-    return internal_func_wrapper
-
-
-@markGroup("General")
-class GrpGeneral:
-    """
-    This is the default group. Contains all commandos that are available to everyone on the server.
-    """
-    userLevel = 0
-    name = "General"
-
-
-################################### Commandos Helpers ########################################
-
-class CDOInteralResponseData:
-    def __init__(self, response: str = "", additionalInfo: OrderedDict = OrderedDict(), reactionFunc=None):
-        self.response = response
-        self.additionalInfo = additionalInfo
-        self.reactionFunc = reactionFunc
-
-
-class CDOFullResponseData:
-    def __init__(self, channel: Channel, cdo: str, internalResponse: CDOInteralResponseData):
-        self.channel = channel
-        self.cdo = cdo
-        self.response = internalResponse.response
-        self.additionalInfo = internalResponse.additionalInfo
-
-    def __str__(self):
-        return f"Posting {self.response} to {self.cdo} with addInfo {self.additionalInfo} to {self.channel}"
-
-
-async def sendResponse(responseData):
-    logger.info(responseData)
-    title = f"Commando {responseData.cdo}"
-    content = responseData.response
-
-    embObj = Embed(title=title, description=content)
-
-    for key, val in responseData.additionalInfo.items():
-        embObj.add_field(name=key, value=val, inline=True)
-
-    await client.send_message(responseData.channel, embed=embObj)
-
-
-def markCommando(cmd: str, group=GrpGeneral, userlevel=None):
-    def internal_func_wrapper(func: callable):
-        async def func_wrapper(**kwargs):
-            responseDataInternal = await func(**kwargs)
-            if not isinstance(responseDataInternal, CDOInteralResponseData):
-                raise TypeError("Commandos need to return a CDOInteralResponseData type!")
-
-            responseData = CDOFullResponseData(kwargs['msg'].channel, kwargs['cdo'], responseDataInternal)
-            msg = await sendResponse(responseData)
-            if responseDataInternal.reactionFunc is not None:
-                await client.wait_for_reaction(message=msg, check=responseDataInternal.reactionFunc)
-            return
-
-        DiscordCommando.addCommando(DiscordCommando(cmd, func_wrapper, func.__doc__, group, userlevel))
-        return func_wrapper
-
-    return internal_func_wrapper
-
-
-async def cmdHandler(msg: Message) -> str:
-    """
-    Receives commands and handles it according to allCommandos. Commandos are automatically parsed from the code.
-    :param msg: message from the discord channel
-    :return:
-    """
-    for cdos in DiscordCommando.allCommandos():
-        if msg.content.startswith(cdos.commando):
-            if msg.author.bot:
-                logger.info("Ignoring {msg.content}, because bot")
-                return
-
-            try:
-                userQuery = DiscordUsers.objects.get(id=msg.author.id)
-                authorUserLevel = userQuery.userLevel
-            except ObjectDoesNotExist:
-                authorUserLevel = 0
-
-            if cdos.userLevel <= authorUserLevel:
-                logger.info(f"Handling {cdos.commando}")
-                kwargs = {'cdo': cdos.commando,
-                          'msg': msg,
-                          'userLevel': authorUserLevel}
-
-                return await cdos.fun(**kwargs)
-            else:
-                responseStr = "I am sorry, you are not allowed to do that"
-                responseData = CDOFullResponseData(msg.channel, cdos.commando, CDOInteralResponseData(responseStr))
-                await sendResponse(responseData)
 
 
 def checkCompetitionParameter(cmdString: str) -> Union[Dict, str]:
@@ -220,17 +49,6 @@ def checkCompetitionParameter(cmdString: str) -> Union[Dict, str]:
         return {"competition": competition_string, "association": parameterSplit[1]}
     except IndexError:
         return {"competition": competition_string, "association": None}
-
-
-################################### Commandos ########################################
-
-@markGroup("Admin")
-class GrpAdmin:
-    """
-    Admin group. Changing of behaviour should fall into this group.
-    """
-    userLevel = 0
-    name = "Admin"
 
 
 ################################### Commandos ########################################
@@ -323,7 +141,7 @@ async def cdoShowMonitoredCompetitions(**kwargs):
     Lists all watched competitions by soccerbot.
     :return: Answer message
     """
-    retString = f"Monitored competitions (react with number emojis to remove.Only the first {len(emojiList)} can " \
+    retString = f"Monitored competitions (react with number emojis to remove.Only the first {len(emojiList())} can " \
                 f"be added this way):\n\n"
     addInfo = OrderedDict()
     compList = []
@@ -384,14 +202,14 @@ async def cdoListCompetitionByCountry(**kwargs):
         retString += comp.clear_name + "\n"
         compList.append(f"{comp.clear_name}#{comp.association.id}")
 
-    retString += f"\n\nReact with according number emoji to add competitions. Only the first {len(emojiList)} can " \
+    retString += f"\n\nReact with according number emoji to add competitions. Only the first {len(emojiList())} can " \
                  f"be added this way"
     responseData.response = retString
 
     def check(reaction, user):
-        if reaction.emoji in emojiList:
+        if reaction.emoji in emojiList():
             try:
-                index = emojiList.index(reaction.emoji)
+                index = emojiList().index(reaction.emoji)
             except ValueError:
                 logger.error(f"{reaction.emoji} not in list!")
                 return False
@@ -422,7 +240,7 @@ async def cdoGetHelp(**kwargs):
     return CDOInteralResponseData(retString, addInfo)
 
 
-@markCommando("!changeEventIcons", GrpAdmin)
+@markCommando("!changeEventIcons")
 async def cdoChangeIcons(**kwargs):
     """
     Allows for changing of icons for match Events. The command with no parameters will return all events,
@@ -466,7 +284,7 @@ async def cdoChangeIcons(**kwargs):
     return CDOInteralResponseData()
 
 
-@markCommando("!showRunningTasks", GrpAdmin)
+@markCommando("!showRunningTasks")
 async def cdoShowRunningTasks(**kwargs):
     """
     Shows all currently running tasks on the server
